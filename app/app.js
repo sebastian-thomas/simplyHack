6b72ff0c5325d2a800d3f7dcce01ede915b4406d
shyjal <shyjal99@gmail.com>
2015-02-28 21:56:28 +0530

    --angular

---
+++ app/app.js
@@ -0,0 +1,52 @@
+(function() {
+
+    var memebers = [{
+        name: 'Me',
+        avatar: 'assets/img/avatar1.png'
+    }, {
+        name: 'Aslam',
+        avatar: 'assets/img/avatar1.png'
+    }, {
+        name: 'Seb',
+        avatar: 'assets/img/avatar2.png'
+    }, {
+        name: 'Ibru',
+        avatar: 'assets/img/avatar3.png'
+    }];
+
+    var app = angular.module('myapp', ['ngRoute']);
+
+    app.config(['$routeProvider', function($routeProvider) {
+            $routeProvider.
+            when('/public', {
+                templateUrl: 'app/views/public_profile.html'
+            }).
+            when('/team', {
+                templateUrl: 'app/views/team.html'
+            }).
+            when('/course', {
+                templateUrl: 'app/views/course.html'
+            }).
+            otherwise({
+                redirectTo: '/public'
+            });
+        }])
+        .controller('discussion', function() {
+
+        })
+        .controller('teamMember', ['$scope', function($scope) {
+            $scope.team = memebers;
+
+        }]).directive('header', function() {
+            return {
+                restrict: 'E',
+                templateUrl: 'app/views/header.html'
+            };
+        })
+        .directive("footer", function() {
+            return {
+                restrict: 'E',
+                templateUrl: 'app/views/footer.html'
+            };
+        });
+})();
---
+++ app/chart.js
@@ -0,0 +1,22 @@
+	var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+
+	var barChartData = {
+		labels : ["Me","Aslam","Seb","Ibru"],
+		datasets : [
+			
+			{
+				fillColor : "rgba(151,187,205,0.5)",
+				strokeColor : "rgba(151,187,205,0.8)",
+				highlightFill : "rgba(151,187,205,0.75)",
+				highlightStroke : "rgba(151,187,205,1)",
+				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+			}
+		]
+
+	}
+	window.onload = function(){
+		var ctx = document.getElementById("canvas").getContext("2d");
+		window.myBar = new Chart(ctx).Bar(barChartData, {
+		
+		});
+	}
