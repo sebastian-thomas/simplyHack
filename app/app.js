c6af9c4ec9111a5d3de0ac5418eb4537e0f6097b
shyjal <shyjal99@gmail.com>
2015-03-01 03:01:04 +0530

    --chartnheader

--- app/app.js
+++ app/app.js
@@ -2,73 +2,96 @@
 
     var memebers = [{
         name: 'Me',
-        avatar: 'assets/img/avatar1.png'
+        avatar: 'assets/img/avatar1.png',
+        teamlevel:80
     }, {
         name: 'Aslam',
-        avatar: 'assets/img/avatar1.png'
+        avatar: 'assets/img/avatar1.png',
+        teamlevel:60
     }, {
         name: 'Seb',
-        avatar: 'assets/img/avatar2.png'
+        avatar: 'assets/img/avatar2.png',
+        teamlevel:70
     }, {
         name: 'Ibru',
-        avatar: 'assets/img/avatar3.png'
+        avatar: 'assets/img/avatar3.png',
+        teamlevel:60
     }];
 
-    var discuss=[{
-        type:'q',
-        text:"what is ng app in angular js"
-    },
-    {
-        type:'s',
-        text:"Iam awsome.!"
+    var discuss = [{
+        type: 'q',
+        username: 'Aslam',
+        text: "what is ng app in angular js",
+        answers: [{
+            username: 'Me',
+            text: 'ng-app directive defines an AngularJS application'
+        }]
+    }, {
+        type: 's',
+        username: 'Me',
+        text: "Iam awsome!"
     }]
 
-    var app = angular.module('myapp', ['course','ngRoute']);
+    var app = angular.module('myapp', ['course', 'ngRoute', 'chart.js']);
 
     app.config(['$routeProvider', function($routeProvider) {
-            $routeProvider.
-            when('/public', {
-                templateUrl: 'app/views/public_profile.html'
-            }).
-            when('/team', {
-                templateUrl: 'app/views/team.html'
-            }).
-            when('/course', {
-                templateUrl: 'app/views/courses.html'
-            }).
-            otherwise({
-                redirectTo: '/public'
-            });
-        }]);
+        $routeProvider.
+        when('/public', {
+            templateUrl: 'app/views/public_profile.html'
+        }).
+        when('/team', {
+            templateUrl: 'app/views/team.html'
+        }).
+        when('/course', {
+            templateUrl: 'app/views/courses.html'
+        }).
+        otherwise({
+            redirectTo: '/public'
+        });
+    }]);
 
-    app.controller('Discussion', function() {
-            var activebtn='q';
-            this.data=discuss;
+    app.controller('Discussion', function($scope) {
+            var activebtn = 'q';
+            this.answerTemp = '';
+            this.data = discuss;
+            this.activateAnswer;
 
-            this.liveDiscuss={
-                type:activebtn,
-                text:''
+            this.liveDiscuss = {
+                type: activebtn,
+                username: 'Me',
+                text: '',
+                answers: []
             };
 
-            this.setActiveBtn=function(num){
-                this.liveDiscuss.type=num;
-                activebtn=num;
+            this.setActiveBtn = function(num) {
+                this.liveDiscuss.type = num;
+                activebtn = num;
             };
-            this.getActiveBtn=function(){
+            this.getActiveBtn = function() {
                 return activebtn;
             };
 
-            this.doSubmit=function(){
-                if(activebtn==null)
-                    alert('Entry must be question or statement');
-                else{
+            this.doSubmit = function() {
+                if (this.liveDiscuss.text.length > 0) {
                     this.data.push(this.liveDiscuss);
                     console.log(this.data);
-                    activebtn='q';
-                    this.liveDiscuss={
-                        type:activebtn,
-                        text:''
+                    activebtn = 'q';
+                    this.liveDiscuss = {
+                        type: activebtn,
+                        username: 'Me',
+                        text: '',
+                        answers: []
                     };
+                }
+            };
+            this.answerEntered = function(index) {
+                if (this.answerTemp.length != 0) {
+                    this.activateAnswer[index] = 0;
+                    this.data[index].answers.push({
+                        username: 'Me',
+                        text: this.answerTemp[index]
+                    });
+                    this.answerTemp[index] = '';
                 }
             };
 
@@ -76,7 +99,12 @@
         .controller('TeamMember', ['$scope', function($scope) {
             $scope.team = memebers;
 
-        }]);
+        }])
+        .controller("BarCtrl", function($scope) {
+
+            $scope.labels = ['Me', 'Aslam', 'Seb', 'Ibru'];
+            $scope.data = [[65, 59, 80, 81]];
+        });
 
     app.directive('header', function() {
             return {
@@ -89,7 +117,21 @@
                 restrict: 'E',
                 templateUrl: 'app/views/footer.html'
             };
-        });
+        }).directive('ngEnter', function() {
+            return function(scope, element, attrs) {
+                element.bind("keydown keypress", function(event) {
+                    if (event.which === 13) {
+                        scope.$apply(function() {
+                            scope.$eval(attrs.ngEnter, {
+                                'event': event
+                            });
+                        });
+
+                        event.preventDefault();
+                    }
+                });
+            };
+        });;
 
 
 })();
